9505f3ff1f19b2a3f1130712b81336307b05ff02
pedrobeppler-cyber <pedro.beppler@aluno.senai.br>
2026-04-10 16:08:06 -0300

    create model aluno

---
+++ demo/src/main/java/com/example/demo/models/aluno.java
@@ -0,0 +1,75 @@
+package com.example.demo.models;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+public class aluno{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    
+    @Column(name = "nome")
+    private String nome;
+    
+    @Column(name = "dataNascimento")
+    private LocalDate dataNascimento;
+    
+    @Column(name = "email")
+    private String  email;
+
+    public aluno() {
+    }
+
+    public aluno(Integer id, String nome, LocalDate dataNascimento, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    
+    
+
+}
+
